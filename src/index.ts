469d7826ff5a5381af4b72de0b524046059d4754
magicdawn <magicdawn@qq.com>
2025-07-03 15:42:50 +0800

    deps: update dev deps

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export { sort } from 'fast-sort'
-
 import { sort } from 'fast-sort'
+
+export { sort } from 'fast-sort'
 
 export type OrderBy<T> = {
   prop: keyof T | ((it: T) => any)
@@ -11,11 +11,7 @@ export type OrderBy<T> = {
  * just like lodash.orderBy API spec
  */
 
-export function fastOrderBy<T>(
-  list: T[],
-  props: OrderBy<T>['prop'][],
-  orders: OrderBy<T>['order'][],
-) {
+export function fastOrderBy<T>(list: T[], props: OrderBy<T>['prop'][], orders: OrderBy<T>['order'][]) {
   if (props.length !== orders.length) {
     throw new Error('props & orders length not match')
   }
@@ -24,10 +20,7 @@ export function fastOrderBy<T>(
     const order = orders[index]
     if (order === 'asc') return { asc: prop }
     else if (order === 'desc') return { desc: prop }
-    return {
-      asc: prop,
-      comparer: order,
-    }
+    return { asc: prop, comparer: order }
   })
 
   return sort(list).by(_by)
